11b968d71dc15033e703e5fd97047e73abbcc60b
woolalal <cedricwookw96@gmail.com>
2022-07-11 17:13:47 +0800

    update text

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -13,7 +13,7 @@ const Profile = () => {
                     Tests
                 </Tab>
                 <Tab eventKey="contact" title="Contact">
-                    Testing
+                    Testing123
                 </Tab>
                 </Tabs>
             </Container>
